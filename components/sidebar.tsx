924635617536fb06b1c6d50bcfc05bb904654b5b
NSDtheKing <dimisiet@gmail.com>
2024-01-21 14:46:41 +0100

    sidebar_v3: animation

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -13,6 +13,7 @@ const Sidebar = () => {
                 <div className="w-[200px] flex  h-[100%] items-center justify-between group relative" >
                     <h1 className=" basic__subtitle ">
                         nos offres
+                        <span className="absolute bg-black bottom-3 h-0.5 left-0 w-0 group-hover:w-[63%] transition-all duration-200 ease-in-out"></span>
 
                      </h1>  
 
@@ -24,9 +25,21 @@ const Sidebar = () => {
             <div className="w-full flex justify-around mt-[100px]">
                 <div className='w-[80%] flex justify-center border-r-2 border-[#9E9E9E] rounded-sm'>
                     <div className='w-[354px] h-[222px]'>
-                        <h1 className='basic__midtitle'>Our expertise </h1>
-                        <h1 className='basic__midtitle'>who we are </h1>
-                        <h1 className='basic__midtitle'>contact</h1>
+                    <h1 className='group cursor-pointer relative text-black hover:text-white px-2 py-1 z-10'>
+                        <span className="absolute inset-0 bg-black rounded-md opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out z-0"></span>
+                        <span className='relative z-10 basic__midtitle'>Our expertise</span>
+                    </h1>
+
+                    <h1 className='group cursor-pointer relative text-black hover:text-white px-2 py-1 z-10'>
+                        <span className="absolute inset-0 bg-black rounded-md opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out z-0"></span>
+                        <span className='relative z-10 basic__midtitle'>who we are</span>
+                    </h1>
+
+                    <h1 className='group cursor-pointer relative text-black hover:text-white px-2 py-1 z-10'>
+                        <span className="absolute inset-0 bg-black rounded-md opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out z-0"></span>
+                        <span className='relative z-10 basic__midtitle'>contact</span>
+                    </h1>
+
                     </div>
                 </div>
                 <div className='flex flex-col items-center '>
